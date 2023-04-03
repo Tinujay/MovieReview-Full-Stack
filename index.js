275503aea01d55fe0284621aefba17a88dfa2c52
run <tinuvazhayil@gmail.com>
2023-04-02 21:15:00 -0400

    added controllers folder

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+require('dotenv').config()
 const express = require('express');
-
 const app = express()
+
+app.use('/movies', require('./controllers/movies'))
+
+
 
 app.get('/', (req, res) => {
     res.send('Homepage')
 })
 
-app.listen(3000, () => {
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+app.listen(process.env.PORT, () => {
     console.log('I am awake!')
 })
